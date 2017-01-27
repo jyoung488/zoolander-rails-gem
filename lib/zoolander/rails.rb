85073d9442aec69a202d589c156bb99ea5e11347
jyoung488 <jyoung488@gmail.com>
2017-01-26 17:29:11 -0800

    Define random Zoolander method

--- lib/zoolander/rails.rb
+++ lib/zoolander/rails.rb
@@ -2,6 +2,41 @@ require "zoolander/rails/version"
 
 module Zoolander
   module Rails
-    # Your code goes here...
+    zoolander_quotes = ["You can read minds?",
+      "I'm pretty sure there's a lot more to life than being really, really, ridiculously good looking.",
+      "It's that damn Hansel! He's so hot right now!",
+      "But why male models?",
+      "I'm sorry that good-looking people like us made you throw up and feel bad about yourself.",
+      "Moisture is the essence of wetness, and wetness is the essence of beauty.",
+      "A week? What, are you having a whack attack? I saw you this afternoon, dum-dum.",
+      "Or are you here to tell me what a bad eugoogoolizer I am?",
+      "I guess you can dere-lick my balls cap-E-tan.",
+      "Listen to your friend Billy Zane, he's a cool dude!",
+      "It's a walk-off!",
+      "Mer-man! [high-pitched cough] Mer-man!",
+      "You is talking loco and I like it!",
+      "Be professionally good looking.",
+      "Mugatu is so hot right now he could take a crap, wrap it in tinfoil, put a couple fish hooks on it and sell it to Queen Elizabeth as earrings.",
+      "Now, this'll be a straight walk-off, old school rules. First model walks; second model duplicates, then elaborates. Okay, boys - let's go to work!",
+      "I'm a hand model, mama. A finger jockey. We think differently than the face and body boys... we're a different breed.", "You think that you're too cool for school, but I have a newsflash for you Walter Cronkite... you aren't.",
+      "How can we be expected to teach children to learn how to read if they can't even fit inside the building?",
+      "They're break-dance fighting.",
+      "A eugoogalizor, one who speaks at funerals. Or did you think I'd be too stupid to know what a eugoogoly was?",
+      "By the way, you were wrong about my outfit. It's the Cheryl Ladd collection and I got it at JC Penney's. On sale!",
+      "Now if you'll excuse me, I have an after-funeral party to attend.",
+      "Let me show you Derelicte. It is a fashion, a way of life inspired by the very homeless, the vagrants, the crack whores that make this wonderful city so unique.",
+      "Well I guess it all started the first time I went through the second grade. I caught my reflection in a spoon while I was eating my cereal, and I remember thinking 'wow, you're ridiculously good looking, maybe you could do that for a career.'",
+      "What say we settle this on the runway...Han-Solo?",
+      "You know, I love modelling. I'd like to do this forever. When I'm 30, I hope I'm still modeling.",
+      "What is this? A center for ants?!",
+      "I think I'm getting the black lung Pop.",
+      "Orange mocha frappuccino!",
+      "At the Derek Zoolander Center For Children Who Can't Read Good And Wanna Learn To Do Other Stuff Good Too, we teach you that there's more to life than being really, really ridiculously good-looking.",
+      "Todd! Are you not aware that I get farty and bloated with a foamy latte!?",
+      "They're IN the computer?"]
+
+      def zoolander
+        zoolander_quotes.sample
+      end
   end
 end
